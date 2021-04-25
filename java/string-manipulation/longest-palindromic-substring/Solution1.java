d886d5dc2a618c02cebceb6e45cccd793381f200
Pranav Gaur <23072645+pranavgaur@users.noreply.github.com>
2021-04-25 20:00:42 +0530

    Update Solution1.java

--- java/string-manipulation/longest-palindromic-substring/Solution1.java
+++ java/string-manipulation/longest-palindromic-substring/Solution1.java
@@ -3,30 +3,30 @@
 class Solution1 {
     public String longestPalindrome(String s) {
         
-		// Varibale to store the length of string
+	// Varibale to store the length of string
         int n = s.length();
         
-		// Edge condition check
+	// Edge condition check
         if(n < 2)
             return s;
         
-		// Table to track the plaindromic string
+	// Table to track the plaindromic string
         int[][] table = new int[n][n];
         
-		// Varibale to store maximum length value
+	// Varibale to store maximum length value
         int max = 1;
 		
-		// Variable to store the starting index of longest plaindromic substring
+	// Variable to store the starting index of longest plaindromic substring
         int start = 0;
         
-		// Loop across and track the strings of length 1 (i.e. diagonal elements)
-		// All single characters are palindrome
+	// Loop across and track the strings of length 1 (i.e. diagonal elements)
+	// All single characters are palindrome
         for(int i = 0; i < n ; i++)
             table[i][i] = 1;
         
-		// Loop across and track the strings of length 2
-		// Check if they are plaindrome or not and mark 
-		// the corresponding [row, col] in table as 1
+	// Loop across and track the strings of length 2
+	// Check if they are plaindrome or not and mark 
+	// the corresponding [row, col] in table as 1
         for(int i=0; i< n-1 ; i++){
             if(s.charAt(i) == s.charAt(i+1)){
                 table[i][i+1]=1;
@@ -35,10 +35,10 @@ class Solution1 {
             }     
         }
         
-		// Iterate through the string for string of length 3 to N(string length)
-		// Check if the first and last characters are same and then check if the
-		// previous cells in table are plaindrome or not. Keep track of max 
-		// length and starting index
+	// Iterate through the string for string of length 3 to N(string length)
+	// Check if the first and last characters are same and then check if the
+	// previous cells in table are plaindrome or not. Keep track of max 
+	// length and starting index
         int gap = 3;
 		
         while(gap <= n){
@@ -56,7 +56,7 @@ class Solution1 {
             gap++;
         }
         
-		// Edge condition if start and max come up to be 0
+	// Edge condition if start and max come up to be 0
         if(start == 0 && max == 0)
             return s.charAt(start)+"";
         
