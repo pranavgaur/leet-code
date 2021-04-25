0faac8860f287cf96e9278b51c41a695801ea65a
Pranav Gaur <23072645+pranavgaur@users.noreply.github.com>
2021-04-25 19:59:00 +0530

    Update Solutions.java

--- java/mathematical-manipukation/prime-number-of-setbits/Solutions.java
+++ java/mathematical-manipukation/prime-number-of-setbits/Solutions.java
@@ -2,29 +2,29 @@
 class Solution {
     public int countPrimeSetBits(int L, int R) {
         
-		// Variable to count the number of prime bit sets
+	// Variable to count the number of prime bit sets
         int count = 0;
         
-		// Loop from start to end 
+	// Loop from start to end 
         while(L <= R){
 		
-			// Check if the current number has prime number of bitsets
-			// Increment the counter if true
-            if(hasPrimeBit(L))
-                count++;
+		// Check if the current number has prime number of bitsets
+		// Increment the counter if true
+        	if(hasPrimeBit(L))
+                	count++;
 				
-            L++;
+            	L++;
         }
         
-		// Return the count value
+	// Return the count value
         return count;
         
     }
     
-	// Variable to check if the number has prime number of bitsets
+    // Variable to check if the number has prime number of bitsets
     Boolean hasPrimeBit(int num) {
 		
-		int bitCount = Integer.bitCount(num);
+	int bitCount = Integer.bitCount(num);
         
         return (x == 2 || x == 3 || x == 5 || x == 7 ||
                 x == 11 || x == 13 || x == 17 || x == 19 ||
